66a232cc276d259d03f2eeaa679002105cf82d7f
Elteclas6021 <juanda12346021@gmail.com>
2025-09-02 14:37:15 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://djsiddz.github.io",
+  site: "https://jdherrerac.github.io",
   base: "/space-ahead",
   integrations: [
     swup({
